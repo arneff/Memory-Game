f128dc90d9b7cff1479a8fccbfeb2dde4f649fad
Aaron Neff <arneff@gmail.com>
2017-09-27 11:06:19 -0600

    Changed timer to display on page

--- js/app.js
+++ js/app.js
@@ -14,11 +14,12 @@ let openCards = [];
 let move = 0;
 let pairs = 0;
 let stars = 0;
-let startTime = 0;
-let endTime = 0;
+let seconds = document.getElementById('sec');
+let minutes = document.getElementById('min');
 let elapsedTime = 0;
+let timer = null;
 /* clears existing cards to prepare for new round */
-function clearCards(){
+function clearCards() {
   $('.card').find('i').remove();
 }
 
@@ -32,9 +33,9 @@ shuffle(cards);
   *   - add each card's HTML to the page
   */
 
- function dealCards(){
+ function dealCards() {
    clearCards();
-   $.each(cards, function(index, value){
+   $.each(cards, function(index, value) {
      let liNode = document.createElement('li'); //create new li element since clearCards() removed it
      liNode.className = 'fa ' + value; //assign class name to li
      $('.card')[index].append(liNode); // assign li to each card
@@ -60,12 +61,12 @@ function shuffle(array) {
 }
 
 //start game time
-$('.deck').one("click", function(){
-     startTime = Date.now();
+$('.deck').one("click", function() {
+     timer = setInterval(time, 1000);
 });
 
 //set up the event listener for a card. If a card is clicked:
- $('.deck').find('.card').click(function(){
+ $('.deck').find('.card').click(function() {
    showCard($(this));
    open($(this));
    match(openCards);
@@ -74,7 +75,7 @@ $('.deck').one("click", function(){
 
  //if player wants to startgame over - refresh page by clicking restart button
 
- $('.restart').click(function(){
+ $('.restart').click(function() {
    location.reload();
  });
 
@@ -85,15 +86,15 @@ $('.deck').one("click", function(){
  }
 
 //add the card to a *list* of "open" cards
-function open(card){
-  if (card.hasClass('open')){
+function open(card) {
+  if (card.hasClass('open')) {
     openCards.push(card.children());
   }
 }
 
 //if the list already has another card, check to see if the two cards match
 
-function match(array){
+function match(array) {
   if (array.length > 1){
     if (openCards[0][0].className  === openCards[1][0].className) {
       console.log("match");
@@ -111,8 +112,8 @@ function match(array){
 }
 
 //if the cards do match, lock the cards in the open position
-function matchLock(){
-  $.each(openCards, function(index, value){
+function matchLock() {
+  $.each(openCards, function(index, value) {
     $(this).parent().addClass('match');
     openCards = [];
   });
@@ -121,14 +122,14 @@ function matchLock(){
 
 
 //if the cards do not match, remove the cards from the list and hide the card's symbol
- function notMatch(){
-   $.each(openCards, function(index, value){
+ function notMatch() {
+   $.each(openCards, function(index, value) {
      $(this).parent().removeClass('open show');
      openCards = [];
    });
 }
 // display move counter on the page
-function moves(){
+function moves() {
     move++;
     let moveTotal =+ move;
     $('.moves').text(moveTotal);
@@ -142,11 +143,11 @@ function moves(){
     starCount(moveTotal);
 }
 
-function starCount(num){
-  if (num < 10){
+function starCount(num) {
+  if (num < 10) {
     stars = 3;
   }
-  else if (num < 20){
+  else if (num < 20) {
     stars = 2;
   }
   else {
@@ -155,24 +156,34 @@ function starCount(num){
 }
 //if all cards have matched, display a message with the final score
 
-function gameOver(){
-  if (pairs === 8){
-    endTime = Date.now();
-    time();
+function gameOver() {
+  if (pairs === 8) {
+    clearInterval(timer);
     let over = confirm('Congratulations, Game Over!\n\n' +
       'You have a star rating of: ' + stars + '. \n\n' +
       'It took you ' + Math.round(elapsedTime) + ' seconds and ' + move + ' moves! \n\n Would you like to play again?');
 
-    if(over === true){
+    if(over === true) {
       location.reload();
     }
     else {
-        alert('Thanks for playing!');
+      alert('Thanks for playing!');
     }
   }
 }
 
-function time(){
-  elapsedTime = (endTime - startTime) / 1000;
-  console.log(elapsedTime);
+function time() {
+  elapsedTime++;
+  seconds.innerHTML = pad(elapsedTime%60);
+  minutes.innerHTML = pad(parseInt(elapsedTime/60));
+}
+
+function pad(num) {
+  let timeOutput = num + "";
+  if(timeOutput.length < 2) {
+    return "0" + timeOutput;
+  }
+  else {
+    return timeOutput;
+  }
 }
